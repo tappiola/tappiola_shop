62ce2bcdc0e5d07eebbcdb08c3cec64044c89865
Tatiana <tatyana.karamorina@easybrain.com>
2020-07-24 17:44:05 +0300

    work on search input

--- frontend/src/components/Header/Search/Search.js
+++ frontend/src/components/Header/Search/Search.js
@@ -6,7 +6,32 @@ import SearchIcon from "../../Icons/SearchIcon";
 
 class Search extends Component {
     state = {
-        searchTerm: ''
+        searchTerm: '',
+        formClasses: ['header-search__div'],
+        inputValue: ''
+    }
+
+    handleSearchTermSubmit = (event) => {
+        event.preventDefault();
+        const value = this.state.inputValue;
+        this.setState({searchTerm: value});
+        this.props.history.push(`/category/search_results?search=${value}`);
+    }
+
+    inputClickHandler = () => {
+        this.setState({formClasses: [...this.state.formClasses, "extended"]});
+    }
+
+    inputLeaveHandler = () => {
+        this.setState({formClasses: this.state.formClasses.filter(x => x !== "extended")});
+    }
+
+    inputChangeHanlder = (event) =>{
+        this.setState({inputValue: event.target.value});
+    }
+
+    clearValueHandler = (event) => {
+        this.setState({searchTerm: '', inputValue: ''});
     }
 
     componentDidMount() {
@@ -15,32 +40,30 @@ class Search extends Component {
 
     componentWillReceiveProps(nextProps, nextValue) {
         if (!nextProps.history.location.search) {
-            this.setState({searchTerm: ''});
-            this.textInput.value = '';
-
+            this.setState({searchTerm: '', inputValue: ''});
         }
-    }
-
-    handleSearchTermSubmit = (event) => {
-        event.preventDefault();
-        const value = this.textInput.value;
-        this.setState({searchTerm: value});
-        this.props.history.push(`/category/search_results?search=${value}`);
     }
 
     render() {
         return (
-            <div className='header__search'>
-                <form name='header-search' onSubmit={this.handleSearchTermSubmit}>
-                    <input className='search-input'
-                           defaultValue={this.state.searchTerm}
-                           ref={input => this.textInput = input}
-                           placeholder="Search"
-                    />
-                    <button className='search-button'>
-                        <SearchIcon/>
-                    </button>
-                </form>
+            <div className='header-search__container'>
+                <div className={this.state.formClasses.join(' ')}>
+                    <form name='header-search__form' onSubmit={this.handleSearchTermSubmit}>
+                        <input className="search-input"
+                               defaultValue={this.state.searchTerm}
+                               placeholder="Search"
+                               onChange={this.inputChangeHanlder}
+                               onFocus={this.inputClickHandler}
+                               onBlur={this.inputLeaveHandler}
+                        />
+                        <div className='search-icon'>
+                            <SearchIcon/>
+                        </div>
+                        <div className={`close-icon ${this.state.inputValue === '' && 'hidden'}`} onClick={this.clearValueHandler}>
+                            <i className="fa fa-times" aria-hidden="true"/>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
