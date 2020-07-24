fbf602925bf459917530ad8cfd9e971c10a6ee61
Tatiana <karamorina@gmail.com>
2020-07-24 23:26:16 +0300

    Change config to serve static content in django

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -124,4 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'shop_public')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'shop_public'),
+)
