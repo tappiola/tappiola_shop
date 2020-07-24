864bd4e19f426140af9764ec95c475d0b26f82d0
Tatiana <karamorina@gmail.com>
2020-07-25 00:12:31 +0300

    Change debug to False

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yx0+_)oj2bgp1!amoe_9joorhzvh22!n$p$o__hi@h@f8$y-!r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
