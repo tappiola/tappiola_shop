30da7373999c11936a44ceebc8de251af4117568
Tatiana <tatyana.karamorina@easybrain.com>
2020-07-26 08:27:42 +0300

    fix home page in safari

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -12,36 +12,46 @@ class Home extends Component {
     render() {
         return (<div className="home">
             <div className="home__banner" id="banner1">
-                <img
-                    src="https://tappiola-shop.s3.eu-west-2.amazonaws.com/home/home1.jpg"
-                    alt="banner1"
-                    onClick={this.imageClickHandler.bind(this, '/category/1/product/18')}
-                />
-                <img
-                    src="https://tappiola-shop.s3.eu-west-2.amazonaws.com/home/home2.jpg"
-                    alt="banner2"
-                    onClick={this.imageClickHandler.bind(this, '/category/1/product/18')}
-                />
-                <img
-                    src="https://tappiola-shop.s3.eu-west-2.amazonaws.com/home/home3.jpg"
-                    alt="banner3"
-                    onClick={this.imageClickHandler.bind(this, '/category/1/product/18')}
-                />
+                <div>
+                    <img
+                        src="https://tappiola-shop.s3.eu-west-2.amazonaws.com/home/home1.jpg"
+                        alt="banner1"
+                        onClick={this.imageClickHandler.bind(this, '/category/1/product/18')}
+                    />
+                </div>
+                <div>
+                    <img
+                        src="https://tappiola-shop.s3.eu-west-2.amazonaws.com/home/home2.jpg"
+                        alt="banner2"
+                        onClick={this.imageClickHandler.bind(this, '/category/1/product/18')}
+                    />
+                </div>
+                <div>
+                    <img
+                        src="https://tappiola-shop.s3.eu-west-2.amazonaws.com/home/home3.jpg"
+                        alt="banner3"
+                        onClick={this.imageClickHandler.bind(this, '/category/1/product/18')}
+                    />
+                </div>
             </div>
             <div className="home__banner-label">Cut from luxe lightweight fabrics in a neutral palette, these softly
                 structured pieces from our & Other Stories are tailored for sun-soaked city days.
             </div>
             <div className="home__banner" id="banner2">
-                <img
-                    src="https://tappiola-shop.s3.eu-west-2.amazonaws.com/home/home4.jpg"
-                    alt="banner4"
-                    onClick={this.imageClickHandler.bind(this, '/category/3/product/23')}
-                />
-                <img
-                    src="https://tappiola-shop.s3.eu-west-2.amazonaws.com/home/home5.jpg"
-                    alt="banner5"
-                    onClick={this.imageClickHandler.bind(this, '/category/3/product/23')}
-                />
+                <div>
+                    <img
+                        src="https://tappiola-shop.s3.eu-west-2.amazonaws.com/home/home4.jpg"
+                        alt="banner4"
+                        onClick={this.imageClickHandler.bind(this, '/category/3/product/23')}
+                    />
+                </div>
+                <div>
+                    <img
+                        src="https://tappiola-shop.s3.eu-west-2.amazonaws.com/home/home5.jpg"
+                        alt="banner5"
+                        onClick={this.imageClickHandler.bind(this, '/category/3/product/23')}
+                    />
+                </div>
             </div>
             <div className="home__brands-label">Build your capsule wardrobe. Select timeless pieces by the best European
                 designers
