e6f459c0d8946f2a7b9264a9a3f05998d76d51ab
Tatiana <tatyana.karamorina@easybrain.com>
2020-07-27 00:53:19 +0300

    Split buildpacks

--- frontend/src/lib/service.js
+++ frontend/src/lib/service.js
@@ -4,50 +4,46 @@ import axios from 'axios';
 const API_HOST = 'https://tappiola-shop-sandbox.herokuapp.com';
 
 export const getCategories = () => {
-
-    return axios.get(API_HOST + '/categories');
+    return axios.get(API_HOST + '/api/categories');
 };
 
 export const getCategoryProducts = (categoryId) => {
-    return axios.get(API_HOST + '/category_products/' + categoryId);
+    return axios.get(API_HOST + '/api/category_products/' + categoryId);
 }
 
 export const getBrandProducts = (designerId) => {
-    return axios.get(API_HOST + '/brand_products/' + designerId);
+    return axios.get(API_HOST + '/api/brand_products/' + designerId);
 }
 
 export const getBrands = () => {
-
-    return axios.get(API_HOST + '/brands');
+    return axios.get(API_HOST + '/api/brands');
 };
 
 export const getBrand = (id) => {
-
-    return axios.get(API_HOST + '/brands/' + id);
+    return axios.get(API_HOST + '/api/brands/' + id);
 };
 
 export const getSaleItems = () => {
-
-    return axios.get(API_HOST + '/sale');
+    return axios.get(API_HOST + '/api/sale');
 };
 
 export const getProducts = (paramsDict) => {
     const params = paramsDict;
-    return axios.get(API_HOST + '/products', {params});
+    return axios.get(API_HOST + '/api/products', {params});
 };
 
 export const getProduct = (id) => {
-    return axios.get(API_HOST + '/products/' + id);
+    return axios.get(API_HOST + '/api/products/' + id);
 };
 
 export const createOrder = (data) => {
-    return axios.post(API_HOST + '/orders/create', data);
+    return axios.post(API_HOST + '/api/orders/create', data);
 };
 
 export const submitOrder = (orderId, data) => {
-    return axios.put(API_HOST + '/orders/submit/' + orderId, data);
+    return axios.put(API_HOST + '/api/orders/submit/' + orderId, data);
 };
 
 export const getOrder = (orderId) => {
-    return axios.get(API_HOST + '/orders/' + orderId);
+    return axios.get(API_HOST + '/api/orders/' + orderId);
 };
