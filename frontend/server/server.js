78bfab72ad244eebe816f700e0e31fe74f6045f8
Tatiana <tatyana.karamorina@easybrain.com>
2020-07-27 13:48:33 +0300

    use *

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
