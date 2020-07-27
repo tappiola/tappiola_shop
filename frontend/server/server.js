8534ab614ac990d80ed26e65343067cbeb16b145
Tatiana <tatyana.karamorina@easybrain.com>
2020-07-27 13:15:27 +0300

    http mredirect

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -8,7 +8,11 @@ const app = express();
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
- });
+
+    if (!req.headers.host.includes('localhost')) {
+        res.redirect('https://' + req.headers.host + req.url);
+    }
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
